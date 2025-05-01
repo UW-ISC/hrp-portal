4004efeb2da9c8749fc324bd7e92af1b0a68943d
Jeff Blancato <blancato@uw.edu>
2025-05-01 15:34:04 -0700

    More plugin updates

--- wp-content/plugins/media-library-assistant/includes/class-mla-core.php
+++ wp-content/plugins/media-library-assistant/includes/class-mla-core.php
@@ -21,7 +21,7 @@ class MLACore {
 	 *
 	 * @var	string
 	 */
-	const CURRENT_MLA_VERSION = '3.25';
+	const CURRENT_MLA_VERSION = '3.26';
 
 	/**
 	 * Current date for Development Versions, empty for production versions
