13cb56c9b275eb101e28ab8246ba3636454c237e
Jeff Blancato <blancato@uw.edu>
2025-11-21 08:53:54 -0800

    Gravity forms plugin update

--- wp-content/plugins/gravityforms/assets/js/dist/scripts-theme.min.asset.php
+++ wp-content/plugins/gravityforms/assets/js/dist/scripts-theme.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('gform_gravityforms_utils', 'jquery', 'wp-polyfill'), 'version' => '7bcd744b0d6fe5aee7fe');
+<?php return array('dependencies' => array('gform_gravityforms_utils', 'jquery', 'wp-polyfill'), 'version' => '4b272a11c8bfd3040b8f');
